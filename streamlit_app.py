1db9621d97d07cf2d2314eeb93b1c8d9017719a0
train-Thomas <hyeon73412@naver.com>
2025-01-06 15:14:15 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -88,9 +88,9 @@ content_data = {
             "https://www.youtube.com/watch?v=3JZ_D3ELwOQ"
         ],
         'texts': [
-            "Label 1 관련 첫 번째 텍스트 내용입니다.",
-            "Label 1 관련 두 번째 텍스트 내용입니다.",
-            "Label 1 관련 세 번째 텍스트 내용입니다."
+            "중국식 냉면은 사실",
+            "맛없습니다",
+            "땅콩 들어간 다른게 더 맛있어요."
         ]
     },
     labels[1]: {
